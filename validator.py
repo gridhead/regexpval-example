70baa4f127ffcec7f2d86f93305137f7c752f8ad
Ankit Sinha <40303711+Ank221199@users.noreply.github.com>
2019-11-11 07:53:39 +0530

    Updated Validator.py
    The other condition for password is been added and is working.
     Condition are:
    Weak Password : 
          ♣ All small letter
          ♣ All capital letter
          ♣ All numbers
          ♣ All Symbols

--- validator.py
+++ validator.py
@@ -59,22 +59,44 @@ def vld_paswd(strpiece):
 
 def vld_paswd(strpiece):
     regex_ezy = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+    regex_ezy_sm = "^[a-z]{8,}"
+    regex_ezy_lg = "^[A-Z]{8,}"
+    regex_ezy_nm = "^[\d]{8,}"
+    regex_ezy_sy = "^[@$!%*?&]{8,}"
     regex_med = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+    regex_med1 = "^(?=.*[a-z])(?=.*[@$!%*?&])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$"
+    regex_med2 = "^(?=.*[A-Z])(?=.*[@$!%*?&])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$"
     regex_hrd = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
     # Minimum eight characters, at least one letter and one number
     # Minimum eight characters, at least one uppercase letter, one lowercase letter and one number
     # Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character
     pats=re.compile(regex_hrd)
     patw=re.compile(regex_ezy)
+    patw_sm=re.compile(regex_ezy_sm)
+    patw_lg=re.compile(regex_ezy_lg)
+    patw_nm=re.compile(regex_ezy_nm)
+    patw_sy=re.compile(regex_ezy_sy)
     patm=re.compile(regex_med)
+    patm1=re.compile(regex_med1)
+    patm2=re.compile(regex_med2)
     mats=re.search(pats, strpiece)
     matw=re.search(patw, strpiece)
+    matw_sm=re.search(patw_sm, strpiece)
+    matw_lg=re.search(patw_lg, strpiece)
+    matw_nm=re.search(patw_nm, strpiece)
+    matw_sy=re.search(patw_sy, strpiece)
     matm=re.search(patm, strpiece)
+    matm1=re.search(patm1, strpiece)
+    matm2=re.search(patm2, strpiece)
     if mats:
         return 3
     elif matm:
         return 2
-    elif matw:
+    elif matm1:
+        return 2
+    elif matm2:
+        return 2
+    elif matw or matw_sm or matw_lg or matw_sy or matw_nm:
         return 1
     else:
         return 0
